0e15d9d4c456b280611403f5cb14a560d6d96649
explddjk <explddjk@gmail.com>
2019-11-26 21:52:24 +0800

    [Video-java] Add full screen show video
    Use GLSurfceView render video frame and Add full screen show video

--- app/src/main/java/com/example/testffmpeg/MainActivity.java
+++ app/src/main/java/com/example/testffmpeg/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.testffmpeg;
 
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
+import android.view.ViewGroup.LayoutParams;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //去掉标题栏
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        //全屏，去掉状态栏
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //rote 90
+        //setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
         setContentView(R.layout.activity_main);
 
     // Example of a call to a native method
@@ -16,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
     tv.setText(stringFromJNI());
     //tv.setText("helodsa");
         //open("/sdcard/Movies/29265300/英雄时刻_20190105-21点12分41s.avi", this);
-        //open("/sdcard/Movies/29265300/独家整理---百度云泄密流出视图新婚大喜发现豪乳伴娘和新郎原来有一腿 2[7-57] - 在綫成人影院 - 草榴社區 - t66y.com.mp4", this);
+        //open("/sdcard/Movies/29265300/video1.mp4", this);
     }
 
     /**
