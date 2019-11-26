47976d63b50a7c1ee7909baba46f6dd63951a992
explddjk <explddjk@gmail.com>
2019-11-26 21:53:34 +0800

    [Video-java] Add full screen show video
    Use GLSurfceView render video frame and Add full screen show video

---
+++ app/src/main/java/com/example/testffmpeg/PLXplayer.java
@@ -0,0 +1,40 @@
+package com.example.testffmpeg;
+
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+import android.util.AttributeSet;
+import android.view.SurfaceHolder;
+
+import java.util.jar.Attributes;
+
+public class PLXplayer extends GLSurfaceView implements Runnable, SurfaceHolder.Callback {
+    public PLXplayer(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public void run() {
+        //Open("/sdcard/Movies/29265300/英雄时刻_20190105-21点12分41s.avi", getHolder().getSurface());
+        Open("/sdcard/Movies/29265300/video1.mp4",
+                getHolder().getSurface());
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder)
+    {
+        new Thread( this ).start();
+    }
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width,
+                               int height)
+    {
+
+    }
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder)
+    {
+
+    }
+
+    public native void Open(String url, Object surface);
+}
